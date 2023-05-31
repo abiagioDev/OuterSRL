2298e9f3245a038e0bbaf2ea8a86e577e1bf3cd4
Andrea Biagioni <andrea.biagioni.work@gmail.com>
2023-05-31 14:58:52 +0200

    Uploading Test

--- index.php
+++ index.php
@@ -40,7 +40,7 @@
         <!--[if lte IE 9]>
             <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="https://browsehappy.com/">upgrade your browser</a> to improve your experience and security.</p>
         <![endif]-->
-
+<!-- TEST FTP RIUSCITO -->
         <!-- main wrapper start -->
         <div class="main-wrapper">
 
